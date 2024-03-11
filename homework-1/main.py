6ef4aa55f5c693d22fa761c705f2742b1b04b057
Oleg Zaslavskii <Zasoleg1995@gmail.com>
2024-03-11 23:09:48 +0300

    Add try

--- homework-1/main.py
+++ homework-1/main.py
@@ -3,45 +3,49 @@ import csv
 import psycopg2
 
 # Подключаемся
-with psycopg2.connect(
+conn =  psycopg2.connect(
         host='localhost',
         database='north',
         user='postgres',
         password='12345'
-) as conn:
-    with conn.cursor() as cur:
+)
 
-        # Заполняем данными таблицу customers
-        with open('north_data\\customers_data.csv', newline='', encoding='windows-1251') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                cur.execute("INSERT into customers VALUES (%s, %s, %s)",
-                            (f"{row['customer_id']}",
-                             f"{row['company_name']}",
-                             f"{row['contact_name']}"))
+# Делаем через try чтобы гарантированно закрыть подключение
+try:
+    with conn:
+        with conn.cursor() as cur:
 
-        # Заполняем данными таблицу employees
-        with open('north_data\\employees_data.csv', newline='', encoding='windows-1251') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                cur.execute("INSERT into employees VALUES (%s, %s, %s, %s, %s, %s)",
-                            (f"{row['employee_id']}",
-                             f"{row['first_name']}",
-                             f"{row['last_name']}",
-                             f"{row['title']}",
-                             f"{row['birth_date']}",
-                             f"{row['notes']}"))
+            # Заполняем данными таблицу customers
+            with open('north_data\\customers_data.csv', newline='', encoding='windows-1251') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    cur.execute("INSERT into customers VALUES (%s, %s, %s)",
+                                (f"{row['customer_id']}",
+                                 f"{row['company_name']}",
+                                 f"{row['contact_name']}"))
 
-        # Заполняем данными таблицу orders
-        with open('north_data\\orders_data.csv', newline='', encoding='windows-1251') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                cur.execute("INSERT into orders VALUES (%s, %s, %s, %s, %s)",
-                            (f"{row['order_id']}",
-                             f"{row['customer_id']}",
-                             f"{row['employee_id']}",
-                             f"{row['order_date']}",
-                             f"{row['ship_city']}"))
+            # Заполняем данными таблицу employees
+            with open('north_data\\employees_data.csv', newline='', encoding='windows-1251') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    cur.execute("INSERT into employees VALUES (%s, %s, %s, %s, %s, %s)",
+                                (f"{row['employee_id']}",
+                                 f"{row['first_name']}",
+                                 f"{row['last_name']}",
+                                 f"{row['title']}",
+                                 f"{row['birth_date']}",
+                                 f"{row['notes']}"))
 
-# Закрываем соединение
-conn.close()
+            # Заполняем данными таблицу orders
+            with open('north_data\\orders_data.csv', newline='', encoding='windows-1251') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    cur.execute("INSERT into orders VALUES (%s, %s, %s, %s, %s)",
+                                (f"{row['order_id']}",
+                                 f"{row['customer_id']}",
+                                 f"{row['employee_id']}",
+                                 f"{row['order_date']}",
+                                 f"{row['ship_city']}"))
+finally:
+    # Закрываем соединение
+    conn.close()
